01ac9ac89eae76a728ff5aea38363375c7e0372f
kenjinote <kenji256@gmail.com>
2017-01-15 11:00:32 +0900

    slightly changed

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,6 @@ TCHAR szClassName[] = TEXT("Window");
 
 BOOL IsConteinUnicodeCharacter(LPCWSTR lpszText)
 {
-	BOOL bReturn;
 	if (!lpszText || !lpszText[0])return FALSE;
 	DWORD dwSize = WideCharToMultiByte(CP_ACP, 0, lpszText, -1, 0, 0, 0, 0);
 	LPSTR lpszTextA = (LPSTR)GlobalAlloc(0, dwSize);
@@ -15,14 +14,7 @@ BOOL IsConteinUnicodeCharacter(LPCWSTR lpszText)
 	LPWSTR lpszTextW = (LPWSTR)GlobalAlloc(0, sizeof(WCHAR)*dwSize);
 	MultiByteToWideChar(CP_ACP, 0, lpszTextA, -1, lpszTextW, dwSize);
 	GlobalFree(lpszTextA);
-	if (lstrcmpW(lpszText, lpszTextW) == 0)
-	{
-		bReturn = FALSE;
-	}
-	else
-	{
-		bReturn = TRUE;
-	}
+	const BOOL bReturn = lstrcmpW(lpszText, lpszTextW) != 0;
 	GlobalFree(lpszTextW);
 	return bReturn;
 }
@@ -34,9 +26,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	switch (msg)
 	{
 	case WM_CREATE:
-		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD, 10, 10, 512, 32, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL, 10, 10, 512, 32, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
 		hStatic = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("STATIC"), 0, WS_VISIBLE | WS_CHILD, 10, 50, 512, 32, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
-		PostMessage(hWnd, WM_COMMAND, EN_CHANGE << (sizeof(WORD) * 8), (LPARAM)hEdit);
+		break;
+	case WM_SETFOCUS:
+		SetFocus(hEdit);
 		break;
 	case WM_COMMAND:
 		if (HIWORD(wParam) == EN_CHANGE)
@@ -44,14 +38,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			DWORD dwSize = GetWindowTextLengthW(hEdit);
 			LPWSTR lpszText = (LPWSTR)GlobalAlloc(0, sizeof(WCHAR)*(dwSize + 1));
 			GetWindowTextW(hEdit, lpszText, dwSize + 1);
-			if (IsConteinUnicodeCharacter(lpszText))
-			{
-				SetWindowText(hStatic, TEXT("Unicode固有文字が含まれています。"));
-			}
-			else
-			{
-				SetWindowText(hStatic, TEXT("Unicode固有文字は含まれていません。"));
-			}
+			SetWindowText(hStatic, IsConteinUnicodeCharacter(lpszText) ? TEXT("Unicode固有文字が含まれています。") : TEXT("Unicode固有文字は含まれていません。"));
 			GlobalFree(lpszText);
 		}
 		break;
@@ -82,7 +69,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int 
 	RegisterClass(&wndclass);
 	HWND hWnd = CreateWindow(
 		szClassName,
-		TEXT("Window"),
+		TEXT("入力された文字列にUnicode固有文字が含まれているか"),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		0,
